782eff66635c7a66c40e1df550329498e9bdef5b
Zhiyong <wiwang@student.ubc.ca>
2021-03-19 00:38:39 -0700

    added unite tests

--- tests/test_pypkgs_zyw.py
+++ tests/test_pypkgs_zyw.py
@@ -1,2 +1,17 @@
 from pypkgs_zyw import __version__
 from pypkgs_zyw import pypkgs_zyw
+import pandas as pd
+
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((pypkgs_zyw.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((pypkgs_zyw.catbind(a, b)).categories == ["but", "character",
+            "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
+
+
+
